c6abe52cd819ec32269c854177efaab0c6615b78
HardBoss07 <matteo@bosshard.net>
2025-05-23 09:41:35 +0200

    init commit with docker images

---
+++ backend/src/main/java/ch/bosshard/matteo/togetherly/TogetherlyApplication.java
@@ -0,0 +1,13 @@
+package ch.bosshard.matteo.togetherly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherlyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/bosshard/matteo/togetherly/TogetherlyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bosshard.matteo.togetherly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
