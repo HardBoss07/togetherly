56d9da14719536e0ed9265283e71953ef594f0c9
HardBoss07 <matteo@bosshard.net>
2025-05-27 13:14:09 +0200

    finally got docker to run properly

--- backend/src/main/java/ch/bosshard/matteo/togetherly/TogetherlyApplication.java
+++ backend/src/main/java/ch/bosshard/matteo/togetherly/TogetherlyApplication.java
@@ -1,7 +1,15 @@
 package ch.bosshard.matteo.togetherly;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication(scanBasePackages = "ch.bosshard.matteo.togetherly")
 public class TogetherlyApplication {
@@ -10,4 +18,13 @@ public class TogetherlyApplication {
 		SpringApplication.run(TogetherlyApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner printDataSourceInfo(Environment env) {
+		return args -> {
+			System.out.println(">>> SPRING DATASOURCE URL: " + env.getProperty("spring.datasource.url"));
+			System.out.println(">>> SPRING DATASOURCE USERNAME: " + env.getProperty("spring.datasource.username"));
+			System.out.println(">>> SPRING DATASOURCE PASSWORD: " + env.getProperty("spring.datasource.password"));
+		};
+
+	}
 }
